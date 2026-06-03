491c91b0ffeb00a496176ed35abfcfd3f2d99ec7
slash006 <ssslashhh@gmail.com>
2026-06-03 10:33:32 +0200

    Added base routes

---
+++ project/resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+<a href="/">return to the homepage</a>
+
+Hello from contact page
+
+</body>
+</html>
--- project/routes/web.php
+++ project/routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+/*Route::get('/contact', function () {
+    return view('contact');
+});*/
+
+Route::view('contact', 'contact');
+
+
