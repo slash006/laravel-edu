597848661d811c1579d80f524b3235ce0133edc8
slash006 <ssslashhh@gmail.com>
2026-06-03 14:00:10 +0200

    Added base session-based storage

--- project/resources/views/ideas.blade.php
+++ project/resources/views/ideas.blade.php
@@ -1,15 +1,35 @@
 <x-layout>
 
+    @dump($ideas)
 
 
-    <form action="">
+    <form action="/ideas" method="POST">
+        @csrf
+
         <div class="col-span-full">
-            <label for="idea" class="block text-sm/6 font-medium text-white">About</label>
+            <label for="idea" class="block text-sm/6 font-medium text-white">New idea</label>
             <div class="mt-2">
-                <textarea id="idea" name="idea" rows="3" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"></textarea>
+                <textarea id="idea" name="idea" rows="3" class="block w-full rounded-md bg-white/20 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/20 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"></textarea>
             </div>
-            <p class="mt-3 text-sm/6 text-gray-400">Write a few sentences idea yourself.</p>
+            <p class="mt-3 text-sm/6 text-gray-400">Have an idea you want to save for later?</p>
         </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+            <button type="button" class="text-sm/6 font-semibold text-white">Cancel</button>
+            <button type="submit" class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Save</button>
+        </div>
+
+        @if(count($ideas))
+            <h3 class="font-bold">Stored ideas</h3>
+            <ul class="mt-6">
+                @foreach($ideas as $idea)
+                    <li class="text-sm">{{$idea}}</li>
+
+                @endforeach
+            </ul>
+        @endif
+
+
     </form>
 
 
--- project/routes/web.php
+++ project/routes/web.php
@@ -3,8 +3,27 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('ideas');
+    return view('ideas', [
+        'ideas' => session()->get('ideas', [])
+    ]);
 });
+
+Route::post('/ideas', function () {
+
+    $idea = request()->idea;
+
+    session()->push('ideas', $idea);
+    return redirect('/');
+
+//    dd(request("idea"));
+});
+
+Route::get('/delete-ideas', function () {
+
+    session()->forget('ideas'); // forget or delete?
+    return redirect('/');
+});
+
 
 
 Route::get('/about', function () {
