91cecb300ca7c72cf845b5700859c787c4110476
slash006 <ssslashhh@gmail.com>
2026-06-08 15:05:47 +0200

    Updated idea controller

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,73 +1,80 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Idea;
 
+
+
+Route::get('/ideas', [IdeaController::class, 'index']);
+Route::get('/ideas/create', [IdeaController::class, 'create']);
+Route::post('/ideas', [IdeaController::class, 'store']);
+Route::get('/ideas/{idea}', [IdeaController::class, 'show']);
+Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit']);
+Route::patch('/ideas/{idea}', [IdeaController::class, 'update']);
+Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy']);
+
 // index
-Route::get('/ideas', function () {
-
-    //    $ideas = session()->get('ideas', []);
-    //    $ideas = DB::table('ideas')->get();
-    $ideas = Idea::all();
-
-    // TODO: check Idea::query
-    //    $condition = request()->query('state', 'pending');
-    //    $ideas = Idea::where('state', $condition)->get();
-    //    dd($ideas);
-
-    return view('ideas.index', [ // ideas/index
-        'ideas' => $ideas
-    ]);
-});
+//Route::get('/ideas', function () {
+//
+//    //    $ideas = session()->get('ideas', []);
+//    //    $ideas = DB::table('ideas')->get();
+//    $ideas = Idea::all();
+//
+//    // TODO: check Idea::query
+//    //    $condition = request()->query('state', 'pending');
+//    //    $ideas = Idea::where('state', $condition)->get();
+//    //    dd($ideas);
+//
+//    return view('ideas.index', [ // ideas/index
+//        'ideas' => $ideas
+//    ]);
+//});
 
 
 // create
-Route::get('/ideas/create', function (Idea $idea) {
+//Route::get('/ideas/create', function () {
+//
+//    return view('ideas.create');
+//
+//});
 
-    return view('ideas.create', [
-        'idea' => $idea,
-        'state' => 'mew'
-    ]);
-
-});
-
-// show
-Route::get('/ideas/{idea}', function (Idea $idea) {
-
-    return view('ideas.show', [
-        'idea' => $idea
-    ]);
-
-});
+//// show
+//Route::get('/ideas/{idea}', function (Idea $idea) {
+//
+//    return view('ideas.show', [
+//        'idea' => $idea
+//    ]);
+//
+//});
 
 // edit
-Route::get('/ideas/{idea}/edit', function (Idea $idea) {
+//Route::get('/ideas/{idea}/edit', function (Idea $idea) {
+//
+//    return view('ideas.edit', [
+//        'idea' => $idea
+//    ]);
+//
+//});
 
-    return view('ideas.edit', [
-        'idea' => $idea
-    ]);
-
-});
-
-// update
-Route::patch('/ideas/{idea}', function (Idea $idea) {
-
-    $idea->update([
-        'description' => request('description')
-    ]);
-
-    return redirect('/ideas/' . $idea->id);
-});
+//// update
+//Route::patch('/ideas/{idea}', function (Idea $idea) {
+//
+//    $idea->update([
+//        'description' => request('description')
+//    ]);
+//
+//    return redirect('/ideas/' . $idea->id);
+//});
 
 
 // delete
-Route::delete('/ideas/{idea}', function (Idea $idea) {
-
-    $idea->delete();
-    return redirect('/ideas');
-
-});
+//Route::delete('/ideas/{idea}', function (Idea $idea) {
+//
+//    $idea->delete();
+//    return redirect('/ideas');
+//
+//});
 
 
 // show
@@ -85,26 +92,26 @@ Route::delete('/ideas/{idea}', function (Idea $idea) {
 //
 //});
 
-Route::post('/ideas', function () {
-
-    $idea = request()->idea;
-
-    /*  DB::table('ideas')->insert([
-          'description' => $idea,
-          'state' => 'pending'
-      ]);*/
-
-    Idea::create(
-        [
-            'description' => $idea,
-            'state' => 'pending'
-        ]
-    );
-
-    //    session()->push('ideas', $idea);
-    return redirect('/ideas');
-    //    dd(request("idea"));
-});
+//Route::post('/ideas', function () {
+//
+//    $idea = request()->idea;
+//
+//    /*  DB::table('ideas')->insert([
+//          'description' => $idea,
+//          'state' => 'pending'
+//      ]);*/
+//
+//    Idea::create(
+//        [
+//            'description' => $idea,
+//            'state' => 'pending'
+//        ]
+//    );
+//
+//    //    session()->push('ideas', $idea);
+//    return redirect('/ideas');
+//    //    dd(request("idea"));
+//});
 
 Route::get('/delete-ideas', function () {
 
