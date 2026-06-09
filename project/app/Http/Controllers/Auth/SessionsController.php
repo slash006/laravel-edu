d867f1d3b0a7a9df4e9875d02f65f890ba753429
slash006 <ssslashhh@gmail.com>
2026-06-09 11:54:08 +0200

    Added missing session controller

---
+++ project/app/Http/Controllers/Auth/SessionsController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SessionsController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (\Auth::attempt($validated)) {
+            $request->session()->regenerate();
+            return redirect('/ideas');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+
+        /*
+        return redirect('/ideas');*/
+    }
+
+    /**
+     * Remove the specified resource from storage.sessions
+     */
+    public function destroy()
+    {
+        \Auth::logout();
+        return redirect('/ideas');
+    }
+}
