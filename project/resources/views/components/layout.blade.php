55fcc8902a64ab86d0c42c06cab8d89bba0bfa25
slash006 <ssslashhh@gmail.com>
2026-06-10 13:35:52 +0200

    Fixed vite hot reload

--- project/resources/views/components/layout.blade.php
+++ project/resources/views/components/layout.blade.php
@@ -9,15 +9,11 @@
 {{--    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>--}}
 
 
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
-
-
     <title>
         {{$title }} | Laravel demo
     </title>
 
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 
 </head>
