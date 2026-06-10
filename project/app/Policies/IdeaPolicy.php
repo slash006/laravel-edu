0b25a27bc561fc182e12ba115635078c4264d90c
slash006 <ssslashhh@gmail.com>
2026-06-10 15:04:09 +0200

    Added missing files

---
+++ project/app/Policies/IdeaPolicy.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Idea;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class IdeaPolicy
+{
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function update(User $user, Idea $idea): bool
+    {
+        return $user->is($idea->user);
+        //        return $user->id === $idea->user_id ? Response::allow() : Response::denyAsNotFound();
+
+    }
+
+    /**
+     * Determine whether the user can create the model.
+     */
+    public function create(User $user): bool
+    {
+        return $user->isAdmin();
+    }
+
+}
