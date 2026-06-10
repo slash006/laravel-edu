666bf805d7b7de50a538463c26323ea94429642b
slash006 <ssslashhh@gmail.com>
2026-06-10 20:28:53 +0200

    Added job/worker queue eqample

--- project/app/Notifications/IdeaPublished.php
+++ project/app/Notifications/IdeaPublished.php
@@ -4,10 +4,11 @@ namespace App\Notifications;
 
 use App\Models\Idea;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class IdeaPublished extends Notification
+class IdeaPublished extends Notification implements ShouldQueue
 {
     use Queueable;
 
