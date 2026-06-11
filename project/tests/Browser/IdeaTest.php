e4801ef2605ad6a4c36d827459e3eccf2539803c
slash006 <ssslashhh@gmail.com>
2026-06-11 10:48:49 +0200

    Updated idea test

--- project/tests/Browser/IdeaTest.php
+++ project/tests/Browser/IdeaTest.php
@@ -1,27 +1,41 @@
 <?php
 
+const TMP_IDEA = [
+    'description' => 'tmp idea',
+    'state' => 'new'
+];
+
 it('shows all ideas', function () {
 
     // given I'm signed in
-
     $this->actingAs($user = \App\Models\User::factory()->create());
     // and I have at least one idea
-    $user->ideas()->create([
-        'description' => 'pest description',
-        'state' => 'temporary'
-    ]);
+    $user->ideas()->create(TMP_IDEA);
     // when I visit /ideas endpoint
-
-    visit('/ideas')->assertSee('pest description');
-    // I should see my ideas
+    visit('/ideas')->assertSee(TMP_IDEA['description']);
 });
-
 
 it('shows a single idea', function () {
 
+    $this->actingAs($user = \App\Models\User::factory()->create());
+    $user->ideas()->create(TMP_IDEA);
+    visit('/ideas/1')->assertSee(TMP_IDEA['description']);
+
 });
 
-
 it('shows an edit form to update an idea', function () {
+
+    app(\Illuminate\Foundation\Vite::class)->useHotFile(storage_path('vite.hot.does.not.exist'));
+    $this->actingAs($user = \App\Models\User::factory()->create());
+    $user->ideas()->create(TMP_IDEA);
+    visit('/ideas/1')->assertSee(TMP_IDEA['description']);
+
+    visit('/ideas/1/edit')
+        ->fill('description', 'new description')
+        ->press('@update-idea-button')
+    ;
+
+    visit('/ideas/1')->assertSee("new description");
+
 
 });
