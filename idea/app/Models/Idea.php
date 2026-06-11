48b6b4b327373401c2b3171dce5a517ce7d5701f
slash006 <ssslashhh@gmail.com>
2026-06-11 13:02:43 +0200

    Added Idea, Step, User models and relations, use pest instead of phpunit

--- idea/app/Models/Idea.php
+++ idea/app/Models/Idea.php
@@ -2,11 +2,36 @@
 
 namespace App\Models;
 
+use App\IdeaStatus;
+use Illuminate\Database\Eloquent\Casts\ArrayObject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Idea extends Model
 {
     /** @use HasFactory<\Database\Factories\IdeaFactory> */
     use HasFactory;
+
+    protected $casts = [
+        'links' => ArrayObject::class,
+        'status' => IdeaStatus::class,
+    ];
+
+    protected $attributes = [
+        "status" => IdeaStatus::PENDING
+    ];
+
+    public function user(): BelongsTo
+    {
+
+        return $this->belongsTo(User::class);
+    }
+
+    public function steps(): HasMany
+    {
+
+        return $this->hasMany(Step::class);
+    }
 }
--- idea/app/Providers/AppServiceProvider.php
+++ idea/app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use PhpParser\Node\Expr\AssignOp\Mod;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::unguard();
+        Model::shouldBeStrict();
+        Model::automaticallyEagerLoadRelationships();
     }
 }
