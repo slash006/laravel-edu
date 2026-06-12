5daa82a4e9ef023b388e264816c8ad2cb00abe7d
slash006 <ssslashhh@gmail.com>
2026-06-12 11:27:16 +0200

    Added alpinejs-based flash message

--- idea/resources/views/components/layout/layout.blade.php
+++ idea/resources/views/components/layout/layout.blade.php
@@ -9,10 +9,25 @@
         {{ $slot }}
     </main>
 
+
+{{--    <div x-data="{show: true}">--}}
+{{--        <p x-text="greeting"></p>--}}
+
+{{--        <input type="text" x-model="greeting">--}}
+
+{{--        <p x-show="show">Show me</p>--}}
+
+{{--        <input type="checkbox" value="show" x-model="show" />--}}
+
+{{--    </div>--}}
+
     @session('success')
-        <div class="bg-primary px-4 py-3 absolute bottom-4 right-4 rounded-lg">
+        <div x-data="{show: true}"
+             x-init="setTimeout(() => show = false, 1000)"
+             x-show="show"
+             x-transition.opacity.duration.300ms
+             class="bg-primary px-4 py-3 absolute bottom-4 right-4 rounded-lg">
             {{$value}}
-            
         </div>
     @endsession
 
